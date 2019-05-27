2d65bb7442f0a3fbb2bbafa73617d64f6ef70b19
Diana Vainberg <dianak90@gmail.com>
2019-05-27 13:45:37 +0100

    Initial commit

---
+++ app/src/main/java/diana/vainberg/helloworldkotlin/MainActivity.kt
@@ -0,0 +1,13 @@
+package diana.vainberg.helloworldkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
